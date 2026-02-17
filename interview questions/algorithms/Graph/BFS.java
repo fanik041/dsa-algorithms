0dde183ff3cdd7a3be3070eacff16a75edf9eaa4
fanik041 <fsanik041@gmail.com>
2026-02-17 14:36:23 -0500

    adding comments to bfs

--- interview questions/algorithms/Graph/BFS.java
+++ interview questions/algorithms/Graph/BFS.java
@@ -2,41 +2,6 @@ package Graph;
 import java.util.*;
 
 public class BFS {
-    /*
-    // Returns the BFS visit order starting from `start`
-    public static List<Integer> bfs(Map<Integer, List<Integer>> graph, int start) {
-        List<Integer> order = new ArrayList<>();
-        Queue<Integer> queue = new ArrayDeque<>();
-        Set<Integer> visited = new HashSet<>();
-
-        visited.add(start);
-        queue.add(start);
-
-        while (!queue.isEmpty()) {
-        int node = queue.poll();
-        order.add(node);
-
-        for (int nei : graph.getOrDefault(node, List.of())) {
-            if (visited.add(nei)) {        // add() returns false if already present
-            queue.add(nei);
-            }
-        }
-        }
-        return order;
-    }
-
-    public static void main(String[] args) {
-        Map<Integer, List<Integer>> g = new HashMap<>();
-        g.put(1, List.of(2, 3));
-        g.put(2, List.of(4, 5));
-        g.put(3, List.of(6));
-        g.put(4, List.of());
-        g.put(5, List.of());
-        g.put(6, List.of());
-
-        System.out.println(bfs(g, 1)); // [1, 2, 3, 4, 5, 6]
-    }
-    */
   
   public static List<Integer> bfs(Map<Integer, List<Integer>> graph, int start) {
     List<Integer> order = new ArrayList<>();
@@ -47,9 +12,18 @@ public class BFS {
     queue.add(start);
 
     while(!queue.isEmpty()){
+        //.poll() removes and returns the head of the queue 
+        // for example in [1, 2, 3], .poll() 
+        // will return 1 and the queue will become [2, 3]
         int node = queue.poll();
         order.add(node);
 
+        //getOrDefault() returns the value for the specified key if it exists, 
+        // otherwise it returns the default value provided as the second argument.
+        // for example, graph.getOrDefault(1, List.of()) 
+        // will return [2, 3] if key 1 exists in the graph,
+        // but if key 1 does not exist in the graph,
+        // it will return an empty list List.of()
         for(int neighbor : graph.getOrDefault(node, List.of())){
             if(visited.add(neighbor)){
                 queue.add(neighbor);
@@ -58,6 +32,17 @@ public class BFS {
     }
     return order;
   }
+
+  /*
+          1
+      /   \
+     2     3
+   /  \     \
+  4    5     6
+  This is an example of BFS traversal of the graph starting from node 1.
+  The BFS order will be: [1, 2, 3, 4, 5, 6]
+
+  */
 
   public static void main(String[] args) {
     Map<Integer, List<Integer>> g = new HashMap<>();
